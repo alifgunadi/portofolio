cbdea29c347f4a7cc1105c548272259bd9fcbe4f
alif-gunadi <alifgunadi1303@gmail.com>
2023-02-13 20:55:05 +0700

    change bg banner

--- src/materi/komponen/pembahasan/content/index.jsx
+++ src/materi/komponen/pembahasan/content/index.jsx
@@ -1,5 +1,5 @@
 import React,{ } from "react";
-import Banner1 from '../content/banner1.jpg';
+import Banner2 from '../content/banner2.jpg';
 import Button from 'react-bootstrap/Button';
 import './style.css';
 
@@ -12,7 +12,7 @@ function Banner()
     <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
         <div className="carousel-inner">   
             <div className="carousel-item active">
-                <img src={ Banner1 } className="d-block w-100" alt='Muhammad Alif Gunadi'/>
+                <img src={ Banner2 } className="d-block w-100" alt='Muhammad Alif Gunadi'/>
                     <div className="carousel-caption">
                         <p className="display-6 para-style">Job Seeker
                         <br /><span className="display-2"><b>Muhammad Alif Gunadi</b></span><br /></p>
